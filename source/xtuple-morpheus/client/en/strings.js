f4f671d01573a2f9d04df96aa72fd1d6b52ec5f3
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-07-17 23:04:51 -0400

    issue #24069: start to morpheus integration

---
+++ source/xtuple-morpheus/client/en/strings.js
@@ -0,0 +1,19 @@
+/*jshint node:true, indent:2, curly:false, eqeqeq:true, immed:true,
+latedef:true, newcap:true, noarg:true, regexp:true, undef:true,
+strict:true, trailing:true, white:true */
+/*global XT:true */
+
+(function () {
+  "use strict";
+
+  var lang = XT.stringsFor("en_US", {
+    "_explanation": "Explanation",
+    "_general": "General",
+    "_go": "Go",
+    "_morpheus": "Morpheus",
+  });
+
+  if (typeof exports !== 'undefined') {
+    exports.language = lang;
+  }
+}());
