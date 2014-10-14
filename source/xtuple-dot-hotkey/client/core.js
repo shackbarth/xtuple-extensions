57a39c22d759b3f41662789c7793f81a3b344f5b
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-10-13 22:32:45 -0400

    add documentation

--- source/xtuple-dot-hotkey/client/core.js
+++ source/xtuple-dot-hotkey/client/core.js
@@ -7,18 +7,35 @@ white:true*/
   "use strict";
 
   XT.extensions.xtuple_dot_hotkey = {
+    /**
+      Capture arbitrary hotkey instructions that start with a dot.
+      In this example, entering .i15002<enter> will drill down into incident # 15002.
+      Note that the hotkey listener only operates if there's no cursor focus on a field.
+    */
     addHotkey: function () {
+
+      /**
+        Capture anything between a dot and an enter and send it to the captureDotHotKey function
+
+        @see http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+      */
       App.prototype.keyCapturePatterns.push({
         method: "captureDotHotKey",
-        // http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
         start: [190], // period
         end: [13] // enter
       });
-      // entering .i15002<enter> will drill down into incident # 15002
+
+      /**
+        Waterfall down an instruction to open a workspace if we understand which business object's
+        workspace we want to open.
+
+        @param {String} data
+          In form i15002, which means that the user wants to drilldown into incident 15002
+      */
       App.prototype.captureDotHotKey = function (data) {
         var capturedId;
-        if (data.charAt(0).toLowerCase() === 'i') {
-          capturedId = data.substring(1);
+        if (data.charAt(0).toLowerCase() === 'i') { // for now only i = Incident is supported
+          capturedId = data.substring(1); // everything after the `i`
           this.$.postbooks.waterfall("onWorkspace", {workspace: "XV.IncidentWorkspace", id: capturedId});
         }
       };
