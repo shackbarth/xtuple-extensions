004c83c2dd5561f22cf49d54f2f382df3593f41d
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-10-22 11:56:18 -0400

    xtuple kittens

---
+++ source/xtuple-kittens/database/source/manifest.js
@@ -0,0 +1,10 @@
+{
+  "name": "xtuple-kittens",
+  "version": "0.0.1",
+  "comment": "Kitten extension",
+  "loadOrder": 999,
+  "dependencies": [],
+  "databaseScripts": [
+    "welcome.sql"
+  ]
+}
